4a3ae9b9e91b321799a7e379d17804c4603453d3
Kare Palokallio <kare.palokallio@tuni.fi>
2026-02-25 20:10:55 +0200

    feat(calculator): initialize calculate method

--- Calculator.java
+++ Calculator.java
@@ -3,4 +3,8 @@ public class Calculator {
     public void runCalculator() {
         System.out.println("Laskin on käynnissä!");
     }
+
+    private double calculate(double num1, double num2, String operator) {
+
+    }
 }
