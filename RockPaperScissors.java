19800059df23b16ff01baaec3d512129aa271bd2
Kare Palokallio <kare.palokallio@tuni.fi>
2026-02-28 18:36:12 +0200

    feat(rps): add options array for rock, paper and scissors

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class RockPaperScissors {
+
+    private static final String[] options = {"rock", "paper", "scissors"};
+
     public static void runGame(Scanner scanner) {
         System.out.println("\nWelcome to Rock-Paper-Scissors Game!");
     }
