4a93368ff1e18e9b335dd67a54be2173a04e7b6a
Kare Palokallio <kare.palokallio@tuni.fi>
2026-03-01 17:35:05 +0200

    feat(main): integrate RockPaperScissors into main menu

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ public class Main {
             System.out.println("\nWelcome!");
             System.out.println("\nWhat would you like to use?");
             System.out.println("1. Calculator");
+            System.out.println("2. Rock-Paper-Scissors game");
             System.out.println("0. Exit");
             System.out.print("\nEnter the number of your choice: ");
 
@@ -18,6 +19,9 @@ public class Main {
             switch (choice) {
                 case "1":
                     Calculator.runCalculator(scanner);
+                    break;
+                case "2":
+                    RockPaperScissors.runGame(scanner);
                     break;
                 case "0":
                     running = false;
