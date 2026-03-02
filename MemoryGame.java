eb085bc91724aeecc498f87d0e1fdab2479290ed
Kare Palokallio <kare.palokallio@tuni.fi>
2026-03-02 19:21:23 +0200

    refactor(MG): make board array final

--- MemoryGame.java
+++ MemoryGame.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class MemoryGame {
 
-    private static String[][] board = new String[4][4];
+    private static final String[][] board = new String[4][4];
     private static boolean[][] revealed = new boolean[4][4];
 
     public static void runMG(Scanner scanner) {
