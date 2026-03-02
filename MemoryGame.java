aa8792272b619c9805a7429ac88b1e7d2763e297
Kare Palokallio <kare.palokallio@tuni.fi>
2026-03-02 19:21:57 +0200

    refactor(MG): make revealed array final

--- MemoryGame.java
+++ MemoryGame.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class MemoryGame {
 
     private static final String[][] board = new String[4][4];
-    private static boolean[][] revealed = new boolean[4][4];
+    private static final boolean[][] revealed = new boolean[4][4];
 
     public static void runMG(Scanner scanner) {
         int found = 0;
